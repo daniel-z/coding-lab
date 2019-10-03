6b907b196568235629e1f9f67279ad21740bfd9d
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-10-03 10:59:45 -0500

    Initial setup and file structure.

---
+++ src/views/home/Home.vue
@@ -0,0 +1,54 @@
+<template src="./Home.html"></template>
+<style lang="scss" src="./Home.scss" scoped></style>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+const codeExperiments = [
+  {
+    name: 'Particles Tracker',
+    repoUrl: 'https://github.com/daniel-z/particles-pointer-tracker',
+    description: "2D animation using canvas and ES6, is a complete refactoring and code improvement of a code sample I found in codepen.",
+    tags: [
+      'ES6',
+      'Webpack',
+      'Babel',
+      'Refactoring'
+    ]
+  },
+    {
+    name: 'Particles Tracker',
+    repoUrl: 'https://github.com/daniel-z/particles-pointer-tracker',
+    description: "2D animation using canvas and ES6, is a complete refactoring and code improvement of a code sample I found in codepen.",
+    tags: [
+      'ES6',
+      'Webpack',
+      'Babel',
+      'Refactoring'
+    ]
+  },
+    {
+    name: 'Particles Tracker',
+    repoUrl: 'https://github.com/daniel-z/particles-pointer-tracker',
+    description: "2D animation using canvas and ES6, is a complete refactoring and code improvement of a code sample I found in codepen.",
+    tags: [
+      'ES6',
+      'Webpack',
+      'Babel',
+      'Refactoring'
+    ]
+  }
+];
+
+export default {
+  name: "home",
+  data: () => {
+    return {
+      codeExperiments,
+    };
+  },
+  components: {
+    // HelloWorld
+  }
+};
+</script>
