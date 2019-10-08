428a010cf5b4d3f7c820c572bcf96a103c804a68
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-10-07 23:34:47 -0500

    Adding hero.

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div> -->
+    <div class="hero">
+      <div class="header-wrapper">
+        <header>
+          <h1>Code Experiments</h1>
+          <h2>By Daniel Zamorano</h2>
+        </header>
+      </div>
+    </div>
     <router-view />
   </div>
 </template>
