7974e7b84dd66de92bba4f58238be00d11e934ec
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-10-08 21:24:41 -0500

    Completing home page styles and content.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,22 @@
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div> -->
+    <div class="hero">
+      <div class="header-wrapper">
+        <header>
+          <h1>Code Experiments</h1>
+          <h2>By Daniel Zamorano</h2>
+        </header>
+      </div>
+    </div>
     <router-view />
+    <footer>
+      <p class="about">
+        <a target="_blank" href="https://www.danielzamorano.pro">
+          Daniel Zamorano / 2019
+        </a>
+      </p>
+    </footer>
   </div>
 </template>
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,44 +4,103 @@
 <script>
 const codeExperiments = [
   {
-    name: "Particles Tracker",
-    repoUrl: "https://github.com/daniel-z/particles-pointer-tracker",
+    name: "Vision Player",
+    links: {
+      code: "https://github.com/daniel-z/visionplayer",
+      preview: "https://visionplayer.danielzamorano.pro/"
+    },
     description:
-      "2D animation using canvas and ES6, is a complete refactoring and code improvement of a code sample I found in codepen.",
-    tags: ["ES6", "Webpack", "Babel", "Refactoring"]
+      "I created a web music player that combines music with HD background images. I decided to experiment using Spotify & Unsplash APIs and learn Angular + Ngrx.",
+    tags: [
+      "Angular",
+      "NGRX",
+      "Babel",
+      "Webpack",
+      "node",
+      "npm",
+      "spotify",
+      "unsplash",
+      "REST",
+      "API",
+      "Saas"
+    ]
   },
   {
     name: "Particles Tracker",
-    repoUrl: "https://github.com/daniel-z/particles-pointer-tracker",
+    links: {
+      code: "https://github.com/daniel-z/particles-pointer-tracker"
+      // preview: "https://github.com/daniel-z/particles-pointer-tracker"
+    },
     description:
-      "2D animation using canvas and ES6, is a complete refactoring and code improvement of a code sample I found in codepen.",
-    tags: ["ES6", "Webpack", "Babel", "Refactoring"]
+      "2D animation using canvas and ES6, is a complete refactoring of a code sample I found in codepen.",
+    tags: ["ES6", "Webpack", "Babel", "Refactoring", "node", "npm", "Saas"]
   },
   {
-    name: "Particles Tracker",
-    repoUrl: "https://github.com/daniel-z/particles-pointer-tracker",
+    name: "PHOTOS API",
+    links: {
+      code: "https://github.com/daniel-z/unsplash-api",
+      preview: "https://photo.danielzamorano.pro"
+    },
     description:
-      "2D animation using canvas and ES6, is a complete refactoring and code improvement of a code sample I found in codepen.",
-    tags: ["ES6", "Webpack", "Babel", "Refactoring"]
+      "New experiment with nodejs api framework: https://feathersjs.com/. I created an api to get HD pictures powered by unsplash.",
+    tags: [
+      "unsplash",
+      "feathers",
+      "ES6",
+      "node",
+      "npm",
+      "Webpack",
+      "Babel",
+      "Refactoring"
+    ]
   },
   {
-    name: "Particles Tracker",
-    repoUrl: "https://github.com/daniel-z/particles-pointer-tracker",
+    name: "CONSOLE TYPER",
+    links: {
+      code: "",
+      preview: "https://danielzamorano.pro/#/experience"
+    },
     description:
-      "2D animation using canvas and ES6, is a complete refactoring and code improvement of a code sample I found in codepen.",
-    tags: ["ES6", "Webpack", "Babel", "Refactoring"]
+      "Funny util written using pure javascript to give a terminal typing effect to any paragraph.",
+    tags: ["ES6", "Fun", "node", "yarn", "Webpack", "Babel"]
+  },
+  {
+    name: "CODE EXPERIEMENTS PAGE",
+    links: {
+      code: "https://github.com/daniel-z/react-audition-ecommerce",
+      preview: "https://stackblitz.com/edit/react-audition-ecommerce"
+    },
+    description:
+      "I decided to add this here as I used it as a prototype to learn VueJS.",
+    tags: ["Vue", "Saas"]
+  },
+  {
+    name: "ECOMMERCE SAMPLE",
+    links: {
+      code: "https://github.com/daniel-z/react-audition-ecommerce",
+      preview: "https://stackblitz.com/edit/react-audition-ecommerce"
+    },
+    description:
+      "Ecommerce page sample used as recruiting exercise using react. Using cloudinary CDN and stackblitz for live coding. I ported this same code to angular.",
+    tags: [
+      "React",
+      "Cloudinary",
+      "CDN",
+      "ES6",
+      "node",
+      "npm",
+      "Webpack",
+      "Babel",
+      "Stackblitz"
+    ]
   }
 ];
 
 export default {
   name: "home",
   data: () => {
-    return {
-      codeExperiments
-    };
+    return { codeExperiments };
   },
-  components: {
-    // HelloWorld
-  }
+  components: {}
 };
 </script>
