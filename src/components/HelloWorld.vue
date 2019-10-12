ab693be80c211dff0f027bbcc616b2e1a2818d2e
Daniel Zamorano <dzamorano@tacitknowledge.com>
2019-10-11 20:31:37 -0500

    adding sample unit test.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,8 @@
-<template></template>
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
 
 <script>
 export default {
@@ -9,5 +13,4 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
